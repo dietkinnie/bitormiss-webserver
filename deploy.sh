d2c1e3df294da240ea7a24e3a5b70d00183399dd
dietkinnie <dietkinnie@gmail.com>
2017-02-26 07:24:51 +0000

    Initial Commit

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ set -e -o verbose #-e: stop on first error
 #rm -rf node_modules
 #npm install
 gulp build
-git add build --force
-git add config/build-config.json --force
-git commit -a -m "Compiled Version"
-#git merge origin/prod -s recursive -X ours -m "Merge master into compiled"
-git push origin HEAD:prod --force
-git reset --hard origin/master
+#git add build --force
+#git add config/build-config.json --force
+#git commit -a -m "Compiled Version"
+###git merge origin/prod -s recursive -X ours -m "Merge master into compiled"
+#git push origin HEAD:prod --force
+#git reset --hard origin/master
